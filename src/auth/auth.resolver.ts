6e7b81e146a76f01b4c8f3be16433c5a7681788d
hatemBay <bayoudhhatem5@gmail.com>
2023-09-21 17:08:00 +0100

    Add local-passport jwt authentication

---
+++ src/auth/auth.resolver.ts
@@ -0,0 +1,20 @@
+import { Resolver, Mutation, Args, Context } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { LoginResponse } from './dto/login-response';
+import { LoginUserInput } from './dto/login-user.input';
+import { GqlAuthGuard } from './gql-auth.guard';
+import { UseGuards } from '@nestjs/common';
+
+@Resolver()
+export class AuthResolver {
+  constructor(private authService: AuthService) {}
+
+  @Mutation(() => LoginResponse)
+  @UseGuards(GqlAuthGuard)
+  login(
+    @Args('loginUserInput') loginUserInput: LoginUserInput,
+    @Context() context,
+  ) {
+    return this.authService.login(context.user);
+  }
+}
